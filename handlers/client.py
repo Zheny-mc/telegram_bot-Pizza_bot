01bd2ae58a0662c1a3ac4cee390001305499d2a7
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2023-07-08 13:33:49 +0300

    Добавление клавиатуры

--- handlers/client.py
+++ handlers/client.py
@@ -1,15 +1,18 @@
 from aiogram import types, Dispatcher
 from create_bot import bot
+from keyboards import kb_client
+
+from aiogram.types import ReplyKeyboardRemove
 
 async def command_start(message :types.Message):
     try:
-        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await bot.send_message(message.from_user.id, 'Приятного аппетита', reply_markup=kb_client)
         await message.delete()
     except:
         await message.reply("Общение с ботом можно продолжить в ЛС: https://t.me/My_Pizza_ShephBot")
 
-async def pizza_open_command(message :types.Message):
-    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+async def pizza_mode_command(message :types.Message):
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00', reply_markup=ReplyKeyboardRemove())
 
 async def pizza_place_command(message :types.Message):
     await bot.send_message(message.from_user.id, 'Ул. Колбасная 15')
@@ -17,6 +20,6 @@ async def pizza_place_command(message :types.Message):
 
 def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
-    dp.register_message_handler(pizza_open_command, commands=['Режим_работы'])
+    dp.register_message_handler(pizza_mode_command, commands=['Режим_работы'])
     dp.register_message_handler(pizza_place_command, commands=['Расположение'])
 
---
+++ keyboards/client_kb.py
@@ -0,0 +1,15 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton #, ReplyKeyboardRemove
+
+b1 = KeyboardButton('/Режим_работы')
+b2 = KeyboardButton('/Расположение')
+b3 = KeyboardButton('/Меню')
+b4 = KeyboardButton('Мое расположение', request_location=True)
+b5 = KeyboardButton('Мой номер', request_contact=True)
+
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True)  # one_time_keyboard=True # скрыть клаву
+# kb_client.add(b1).add(b2).add(b3)
+kb_client.add(b1).add(b2).add(b3).row(b4, b5)
+# kb_client.row(b1, b2, b3)
+# kb_client.add(b1).add(b2).insert(b3)
+
+
