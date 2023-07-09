434db6b5f4feed4534449f2253897330a125f8f4
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2023-07-09 10:04:51 +0300

    Машина состояний настроена

--- create_bot.py
+++ create_bot.py
@@ -1,8 +1,11 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 
 with open('token.txt') as f:
     TOKEN = f.readline().strip()
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
