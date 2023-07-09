2e438663bee8537e0c717089c8535223f54bbf4f
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2023-07-09 10:32:38 +0300

    Добавление отмены операции - добавления пиццы

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,4 +1,5 @@
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
 
@@ -50,6 +51,12 @@ async def load_price(message: types.Message, state: FSMContext):
 
     await state.finish()
 
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Отмена добавления пиццы')
 
 def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
@@ -57,3 +64,5 @@ def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
+    dp.register_message_handler(cancel_handler, state='*', commands='отмена')
+    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
