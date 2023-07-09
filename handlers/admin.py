588c89f4fcd0c927a50d9ca166151dd7c268d6b1
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2023-07-09 11:17:42 +0300

    Ограничение доступа. Доступ только для администраторов группы.

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,8 +2,9 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
-
 from create_bot import bot
+
+ID = None
 
 class FSMAdmin(StatesGroup):
     photo = State()
@@ -12,51 +13,63 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
+async def make_chages_command(message : types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, 'Слушаю:')
+    await message.delete()
+
 # commands='Загрузить', state=None
 async def cm_start(message: types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply('Загрузи фото')
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply('Загрузи фото')
 
 
 # content_types=['photo'], state=FSMAdmin.photo
 async def load_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['photo'] = message.photo[0].file_id
-    await FSMAdmin.next()
-    await message.reply('Теперь введи название')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply('Теперь введи название')
 
 # state=FSMAdmin.name
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-    await FSMAdmin.next()
-    await message.reply('Введи описание')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Введи описание')
 
 
 # state=FSMAdmin.description
 async def load_description(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['description'] = message.text
-    await FSMAdmin.next()
-    await message.reply('Укажи цену')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Укажи цену')
 
 
 # state=FSMAdmin.price
 async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = float(message.text)
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text)
 
-    async with state.proxy() as data:
-        await message.reply(str(data))
+        async with state.proxy() as data:
+            await message.reply(str(data))
 
-    await state.finish()
+        await state.finish()
 
 async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('Отмена добавления пиццы')
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('Отмена добавления пиццы')
 
 def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
@@ -66,3 +79,4 @@ def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state='*', commands='отмена')
     dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
+    dp.register_message_handler(make_chages_command, commands=['moderator'], is_chat_admin=True)
