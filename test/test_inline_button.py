9ec96b288127e8a20fba7cde87d7ed1c3a7cbc9f
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2023-07-14 17:28:09 +0300

    Мое голосование.

---
+++ test/test_inline_button.py
@@ -0,0 +1,61 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.filters import Text
+from aiogram.utils import executor
+import os
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+with open('../token.txt') as f:
+    TOKEN = f.readline().strip()
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# url_kb = InlineKeyboardMarkup(row_width=4)
+# url_button = InlineKeyboardButton(text='ютуб', url='https://www.youtube.com/')
+# url_button2 = InlineKeyboardButton(text='гугл', url='https://www.google.com/?hl=Ru')
+# url_kb.add(url_button, url_button2)
+#
+# # инлайн сслыки
+# create_button = lambda text, url: InlineKeyboardButton(text=text, url=url)
+# text_button = (('ютуб1', 'https://www.youtube.com/'),
+#                ('ютуб2', 'https://www.youtube.com/'),
+#                ('ютуб3', 'https://www.youtube.com/'))
+#
+# url_buttons = [create_button(i[0], i[1]) for i in text_button]
+# url_kb.row(*url_buttons)
+#
+# url_kb.insert(create_button('google2', 'https://www.google.com/'))
+#
+# @dp.message_handler(commands='ссылки')
+# async def url_command(message: types.Message):
+#     await message.answer('Links: ', reply_markup=url_kb)
+
+
+# инлайн кнопки
+
+question = {True: 0, False: 0}
+
+in_kb = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Like', callback_data='like_1'),
+                                              InlineKeyboardButton(text='DizLike', callback_data='like_-1'))
+
+@dp.message_handler(commands='test')
+async def test_commands(message: types.Message):
+    await message.answer('Inline button:', reply_markup=in_kb)
+
+@dp.callback_query_handler(Text(startswith='like_'))
+async def www_cal(callback_message: types.CallbackQuery):
+    res = int(callback_message.data.split('_')[1])
+    question[res > 0] += 1
+    await callback_message.answer(str(question), show_alert=True)
+
+
+
+# await callback_message.answer()
+# await callback_message.answer('Кнопка нажата', show_alert=True)
+
+
+
+
+executor.start_polling(dp, skip_updates=True)
